81dcb0446c39cc83b391dd408d5bf10796285d4e
Crazy5202 <maximkuts23@gmail.com>
2025-10-12 21:23:13 +0300

    Added solver functionality for step 1, graph

--- Lab5/Files/solver.py
+++ Lab5/Files/solver.py
@@ -1,8 +1,55 @@
 from tridiag import TRIDIAG_SOLVER
 
-a = [0, -1, -9, -1, 9]
-b = [-6, 13, -15, -7, -18]
-c = [5, 6, -4, 1, 0]
-d = [51, 100, -12, 47, -90]
+import math
+import os
 
-print(TRIDIAG_SOLVER(a,b,c,d).solve())
+# a = [0, -1, -9, -1, 9]
+# b = [-6, 13, -15, -7, -18]
+# c = [5, 6, -4, 1, 0]
+# d = [51, 100, -12, 47, -90]
+
+# print(TRIDIAG_SOLVER(a,b,c,d).solve())
+
+# Вариант 7
+class PARAB_SOLVER:
+    def __init__(self, x_steps = 10, scheme_type: int = 1, approx_type: int = 1):
+        """
+        Схема scheme_type: 1 - явная, 2 - неявная, 3 - Кранка-Николсона.
+        
+        Аппроксимация approx_type: 1 - 1п2т, 2 - 2п3т, 3 - 2п2т.
+        """
+        if scheme_type not in [1,2,3] and approx_type not in [1,2,3]:
+            raise ValueError("Неверно указаны параметры решателя!")
+        
+        self._scheme = scheme_type
+        self._approx = approx_type
+        self._n = x_steps
+
+        self._xd = math.pi / x_steps
+        self._td = 0.25*self._xd**2
+
+        self._start_cond = lambda x: math.sin(x)
+        self._anal_sol = lambda x, t: math.exp(-0.5*t)*math.sin(x)
+
+    def _write_res(self, t:float):
+        PATH = os.path.split(os.path.realpath(__file__))[0] + '/t' + str(round(t,5)) + ".txt"
+        with open(PATH, "w") as f:
+            for i in range (self._n+1):
+                cur_x = i*self._xd
+                f.write(str(cur_x) + ' ' + str(self._u[i]) + ' ' + str(self._anal_sol(cur_x, t)) + '\n')
+
+    def solve(self):
+        self._u = [self._start_cond(i*self._xd) for i in range(0, self._n+1)]
+        if self._scheme == 1:
+            for i in range(1, self._n):
+                self._u[i] = self._u[i] + self._td*(self._u[i+1]-2*self._u[i]+self._u[i-1])/self._xd**2 
+                + 0.5*self._td*math.exp(-0.5*self._td)*math.cos(self._xd)
+            self._u[0] = self._u[1] - self._xd*math.exp(-0.5*self._td)
+            self._u[self._n] = self._u[self._n-1] - self._xd*math.exp(-0.5*self._td)
+            self._write_res(self._td)
+        
+
+if __name__ == "__main__":
+    solver = PARAB_SOLVER()
+    solver.solve()
+    print("DONE!")
---
+++ Lab5/Files/visual.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import os
+
+PATH = os.path.split(os.path.realpath(__file__))[0]
+
+data_files = [file for file in os.listdir(PATH) if len(file)>1 and file[0]=='t' and file[1].isdigit() and file.endswith(".txt")]
+
+for file in data_files:
+    full_path = os.path.join(PATH, file)
+
+    x = []; u_solved = []; u_true = []
+    with open(full_path, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            line_stripped = line.strip().split(' ')
+            x.append(float(line_stripped[0]))
+            u_solved.append(float(line_stripped[1]))
+            u_true.append(float(line_stripped[2]))
+        plt.figure()
+        plt.title("График в момент времени t=" + file.removesuffix('.txt')[1:])
+
+        plt.plot(x, u_solved, marker='o', linestyle='-', color='red', label='solved')
+        plt.plot(x, u_true, marker='o', linestyle='--', color='blue', label='true')
+
+        plt.xlabel('x')
+        plt.ylabel('u')
+        
+        plt.legend()
+        plt.grid()
+        plt.show(block=False)
+
+while plt.get_fignums():
+    plt.pause(1)
